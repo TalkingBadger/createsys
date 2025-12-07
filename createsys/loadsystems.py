986e83d822a57d9e53f5696800e05b833a0f9522
Arne Beckmann <arne.beckmann@campus.lmu.de>
2025-12-08 00:52:53 +0100

    Converted posix path to str for openmm compatibility

--- createsys/loadsystems.py
+++ createsys/loadsystems.py
@@ -6,19 +6,19 @@ def get_pdbfilename_HCNH2O(seed: int):
     seed = int(seed)
     if seed not in range(20):
         raise ValueError("HCN/H2O system is only available for seeds between 0 and 19")
-    return files(data_base_dir) / f"hcnh2o/hcn_h2o_{seed:02}.pdb"
+    return str(files(data_base_dir) / f"hcnh2o/hcn_h2o_{seed:02}.pdb")
 
 def get_pdbfilename_fixed_26HCN10H2O(seed: int, qm_only=False):
     seed = int(seed)
     if seed not in range(19):
         raise ValueError("fixed ratio HCN/H2O system is only available for seeds between 0 and 18")
-    return files(data_base_dir) / f"hcn26h2o10/{'qm' if qm_only else ''}configs/{seed:02}.pdb"
+    return str(files(data_base_dir) / f"hcn26h2o10/{'qm' if qm_only else ''}configs/{seed:02}.pdb")
 
 def get_qmatoms_HCNH2O(seed: int):
     seed = int(seed)
     if seed not in range(20):
         raise ValueError("HCN/H2O system is only available for seeds between 0 and 19")
-    return files(data_base_dir) / f"hcnh2o/qm_atoms_{seed:02}"
+    return str(files(data_base_dir) / f"hcnh2o/qm_atoms_{seed:02}")
 
 def get_pdbfilename_nucleobase(nucleobase: str, seed: int):
     seed = int(seed)
@@ -28,7 +28,7 @@ def get_pdbfilename_nucleobase(nucleobase: str, seed: int):
     if letter not in "ACGT":
         raise ValueError(f"Nucleobase {nucleobase} is not one of (A)denine, (C)ytosin, (T)hymine, nor (G)uanine")
     id = f"{nucleobase[0].upper()}{seed:02}"
-    return files(data_base_dir) / f"nucleobase/F_{id}.pdb"
+    return str(files(data_base_dir) / f"nucleobase/F_{id}.pdb")
 
 
 if __name__ == "__main__":
