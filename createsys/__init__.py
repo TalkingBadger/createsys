fd9a692ba8ef7923b629f8a90889fcb95a9ed527
Arne Beckmann <arne.beckmann@campus.lmu.de>
2026-01-09 22:06:07 +0100

    Added some top level imports

--- createsys/__init__.py
+++ createsys/__init__.py
@@ -1,8 +1,9 @@
 from .interfaces.interfacePackmol import packmolBuilder, packmolStructure
 
-from .pdbutils import createTopologyAndForcefieldFromPDB, xyz2pdb 
+from .pdbutils import createTopologyAndForcefieldFromPDB, xyz2pdb, getAtomIndicesFromResidueNames
 from . import loadsystems
 from . import pdbutils
+from .equilibration_utils import Equilibrator
 
 
 __all__ = [
